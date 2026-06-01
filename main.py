22fcf1c725a5e06640b5cd9984ffe9578cda079d
manas <manasacharya1702@gmail.com>
2026-06-01 12:02:26 +0530

    fixes

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ class MyVoiceAgent(Agent):
 async def start_session(context: JobContext):
     specialization = load_specializations()
 
-    logging.info(f"📞 New call started - Room: {context.room.name}")
+    
     model = GeminiRealtime(
         model="gemini-2.5-flash-native-audio-preview-12-2025",
         api_key=os.getenv("GOOGLE_API_KEY"),
